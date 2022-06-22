b7be5ee92b12cc34d0566dfc0d911e47766c46f8
Kristijan Kusic <wi21b013@technikum-wien.at>
2022-06-22 18:12:38 +0200

    Initial commit

---
+++ src/main/java/at/technikum/apidatetime/ApiDatetimeApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.apidatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDatetimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDatetimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/apidatetime/ApiDatetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.apidatetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDatetimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
